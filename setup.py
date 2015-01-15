b4911e226e88b8f9d07ee2545948d59b71111633
Patrick Altman <paltman@gmail.com>
2015-01-15 14:31:22 -0600

    Version bump

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     description="",
     name="pinax-types",
     long_description=read("README.rst"),
-    version="0.4",
+    version="0.5",
     url="http://pinax-types.rtfd.org/",
     license="MIT",
     packages=find_packages(),
