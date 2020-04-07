ed28ab934cb2826bdd133f2c1ed653c047fa4fea
Mfon Eti-mfon <mfonetimfon@gmail.com>
2020-04-07 01:26:43 +0100

    Update version number

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-VERSION = "1.0.0"
+VERSION = "2.0.0"
 LONG_DESCRIPTION = """
 .. image:: http://pinaxproject.com/pinax-design/patches/pinax-types.svg
     :target: https://pypi.python.org/pypi/pinax-types/
